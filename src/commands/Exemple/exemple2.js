694175b81f18f952a1af6eaec5592381d184ec75
Azrog <78275913+azrogdev@users.noreply.github.com>
2021-02-27 13:24:56 +0100

    Create exemple2.js

---
+++ src/commands/Exemple/exemple2.js
@@ -0,0 +1,11 @@
+// Dexuième utilisation : 
+module.exports.run = (client, message, args) => {
+  if(!args[0]) return
+    message.channel.send("un exemple...!" + `args[0]`) // code de la commande
+}
+module.exports.help = {
+    name: "exemple2", // nom de la commande
+    description: "Renvoie le deuxième exemple", // description de la commande
+    args: true, // utilise des arguments
+    usage: "<test>" // si args = true , il faut spécifier l'usage de la commande
+}
