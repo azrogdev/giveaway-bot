f1edf46bc78ba676d5849b54b996d71757897bf2
Azrog <78275913+azrogdev@users.noreply.github.com>
2021-02-27 13:25:41 +0100

    Update exemple1.js

--- src/commands/Exemple/exemple1.js
+++ src/commands/Exemple/exemple1.js
@@ -3,7 +3,7 @@ module.exports.run = (client, message, args) => {
     message.channel.send("un exemple...!") // code de la commande
 }
 module.exports.help = {
-    name: "exemple", // nom de la commande
-    description: "Renvoie un exemple", // description de la commande
+    name: "exemple1", // nom de la commande
+    description: "Renvoie le premier exemple", // description de la commande
     args: false // n'utilise pas d'arguments
 }
