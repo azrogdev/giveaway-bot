e331c21a929f44ab894cb1955ba2c62ddaa74a24
Azrog <78275913+azrogdev@users.noreply.github.com>
2021-02-27 13:41:08 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 const { Client, Collection } = require("discord.js")
 const { readdirSync } = require("fs")
-
+const { get } = require("http")
 const { TOKEN, PREFIX } = require("./config")
 
-const client = new Client()
+const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 client.commands = new Collection()
+
 const LoadCommands = (dir = "./commands/") => {
     readdirSync(dir).forEach(dirs => {
         const commands = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith(".js"))
@@ -16,24 +17,22 @@ const LoadCommands = (dir = "./commands/") => {
         }
     })
 }
+
+const LoadEvents = (dir = "./events/") => {
+    readdirSync(dir).forEach(dirs => {
+        const events = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith(".js"))
+
+        for (const event of events) {
+            const evt = require(`${dir}/${dirs}/${event}`)
+            const evtName = event.split(".")[0]
+            client.on(evtName, evt.bind(null, client))
+            console.log("Evenement chargé: " + ` ${evtName}`)
+        }
+    })
+}
+
+
 LoadCommands()
-client.on('message', message => {
-    if(!message.content.startsWith(PREFIX) || message.author.bot ) return
-
-    const args = message.content.slice(PREFIX.length).split(/ +/)
-    const commandName = args.shift().toLowerCase()
-
-    if(!client.commands.has(commandName)) return
-    const command = client.commands.get(commandName)
-
-    if(command.help.args && !args.length) {
-        let noArgsReply = `Il faut des arguments pour cette commande ${message.author}`
-
-        if(command.help.usage) noArgsReply += `\n Voici comment utiliser la commande : \`${PREFIX}${command.help.name} ${command.help.usage}\``
-        return message.channel.send(noArgsReply)
-    }
-
-    command.run(client, message, args)
-})
+LoadEvents()
 
 client.login(TOKEN)
